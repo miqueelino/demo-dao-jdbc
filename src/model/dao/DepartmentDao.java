fd75a0dddbf9cb93114c13e7f45632fd1012391c
João Miquelino <157494621+miqueelino@users.noreply.github.com>
2025-09-17 16:45:37 -0300

    Update DepartmentDao.java

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,18 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
+//interface com os metodos que a implementaçao DepartmentDaoJDBC deve implementar
 public interface DepartmentDao {
 
+//CRUD
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
 }
+

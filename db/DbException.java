54e474c9329b4163bea5fe419833bd396b844069
miqueelino <joao.jnunes@mercadolivre.com>
2025-09-17 15:39:49 -0300

    Department entity class

---
+++ db/DbException.java
@@ -0,0 +1,10 @@
+public class DbException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DbException(String msg) {
+		super(msg);
+	}
+
+
+}
---
+++ src/aplication/Program.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Program {
+
+    public static void main(String[] args){
+
+        Department dep = new Department(1, "Books");
+        System.out.println(dep);
+
+    }
+
+}

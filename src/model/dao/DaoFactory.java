280b166d6a00f025f3dde57ae3b945aa584af78a
João Miquelino <157494621+miqueelino@users.noreply.github.com>
2025-09-17 16:45:22 -0300

    Update DaoFactory.java

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,12 @@
 package model.dao;
 
+import model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
+
+    //metodo estatico que retorna uma instancia de SellerDaoJDBC
+    public static SellerDao creatSellerDao(){
+        return new SellerDaoJDBC();
+    }
 
 }

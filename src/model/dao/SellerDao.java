7cc3b9deda9b1346097b53097c56c7fadbedf435
João Miquelino <157494621+miqueelino@users.noreply.github.com>
2025-09-17 16:45:52 -0300

    Update SellerDao.java

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,18 @@
-package model;
+package model.dao;
 
+import java.util.List;
+
+import model.entities.Seller;
+
+//Interface com os metodos que a implementaçao SellerDaoJDBC deve implementar
 public interface SellerDao {
 
+//CRUD
+    void insert(Seller obj);
+    void update (Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+
 }
+

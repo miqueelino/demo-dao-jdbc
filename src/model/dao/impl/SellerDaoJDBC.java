fb8bc9c6db4d46717706ea6b38c8befb12327dd1
João Miquelino <157494621+miqueelino@users.noreply.github.com>
2025-09-17 16:45:04 -0300

    Update SellerDaoJDBC.java

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,5 +1,42 @@
 package model.dao.impl;
 
-public class SellerDaoJDBC {
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+//implementaçao da interface SellerDao que protege os metodos que devem ser implementados
+public class SellerDaoJDBC implements SellerDao{
+
+    @Override
+    public void insert(Seller obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void update(Seller obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
 
 }

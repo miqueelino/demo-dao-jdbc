335b1f264051398ad884cb97846b6e5189d67b65
miqueelino <joao.jnunes@mercadolivre.com>
2025-09-17 16:06:31 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,13 +1,21 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args){
 
         Department dep = new Department(1, "Books");
+
+        Seller seller = new Seller(1, "Jon", "jon@gmail.com", new Date(),3000.0);
+        System.out.println(seller);
         System.out.println(dep);
+
+
 
     }
 
